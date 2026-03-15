58802d3057b13809365f9a2ea2f07a3f4fa4735a
oniichanx <aom1012@hotmail.com>
2026-03-15 18:11:59 +0700

    index

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const preview = document.getElementById("preview");
 const previewImg = document.getElementById("preview-img");
 
 images.forEach(img => {
-    img.onclick = () => {
+    img.addEventListener("click", () => {
         preview.style.display = "flex";
         previewImg.src = img.src;
+    });
+});
+
+preview.addEventListener("click", (e) => {
+    if (e.target !== previewImg) {
+        preview.style.display = "none";
     }
 });
 
-preview.onclick = () => {
-    preview.style.display = "none";
-};
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        preview.style.display = "none";
+    }
+});
